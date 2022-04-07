0ff814411b94892966e8d2ac439e180418f20404
ReeseJones <reesedrjones@gmail.com>
2022-04-07 12:15:56 -0700

    moved creation of pixi app into the effect

--- src/hooks/usePixiApp.tsx
+++ src/hooks/usePixiApp.tsx
@@ -1,31 +1,45 @@
 import * as React from "react";
 import * as PIXI from "pixi.js";
 import { useEffect, useRef, useState } from "react";
+import { StarField } from "../starField/starFieldApp";
 
 export const usePixiApp = () => {
     const [ appContainer, setAppContainer ] = useState<HTMLElement | null>(null);
 
-    const pixiApp = useRef(new PIXI.Application({
-        width: 640,
-        height: 360,
-        resolution: window.devicePixelRatio || 1,
-        backgroundColor: 0x1099bb
-    }));
+    const pixiApp = useRef<PIXI.Application | null>(null);
 
     useEffect(() => {
+        console.log("Creating new pixi app");
+        pixiApp.current = new PIXI.Application({
+            resolution: window.devicePixelRatio || 1,
+            backgroundColor: 0
+        });
         pixiApp.current.view.classList.add("hero-effect");
+
+        //creating starField ont pixi app
+        setTimeout(() => {const starField = new StarField(pixiApp.current as PIXI.Application);}, 1000);
+
+        return () => {
+            console.log("destroying pixi app");
+            pixiApp.current?.destroy(true, true);
+            pixiApp.current = null;
+        }
     },
     []);
 
     useEffect(() => {
-        if (appContainer !== null) {
+        if (appContainer !== null && pixiApp.current) {
+            console.log("Added pixi view");
             pixiApp.current.resizeTo = appContainer;
             appContainer.appendChild(pixiApp.current.view);
-            setTimeout(() => pixiApp.current.resize(), 1);
+            setTimeout(() => pixiApp.current?.resize(), 1);
         }
-
+ 
         return () => {
-            appContainer?.removeChild(pixiApp.current.view);
+            if(pixiApp.current && appContainer) {
+                console.log("removed pixi view");
+                appContainer.removeChild(pixiApp.current.view);
+            }
         };
     }, [ appContainer ]);
 
@@ -36,5 +50,5 @@ export const usePixiApp = () => {
         }
     }, []);
 
-    return [ containerMounting, pixiApp.current ] as const;
+    return [ containerMounting, pixiApp ] as const;
 }
